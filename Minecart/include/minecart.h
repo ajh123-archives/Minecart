45f28abb2b24e697158732bbc3f2269d113e659c
ajh123 <ajh123@users.noreply.github.com>
2022-12-11 20:13:13 +0000

    Begin engine

---
+++ Minecart/include/minecart.h
@@ -0,0 +1,12 @@
+#ifndef MINECART_H
+#define MINECART_H
+
+namespace minecart
+{
+    namespace testing
+    {
+        void printSomething();
+    }
+}
+
+#endif // MINECART_H
---
+++ Minecart/src/minecart.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "things.h"
+
+namespace minecart
+{
+    namespace testing
+    {
+        void printSomething() {
+            std::cout << "Testing!: " << someString << std::endl;
+        }
+    }
+}
